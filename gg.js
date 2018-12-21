5fbbb04b602e411bc22d20805982611982fe53ca
wangdefu <258486434@qq.com>
2018-12-21 11:39:49 +0800

    wrote a gg file

---
+++ gg.js
@@ -0,0 +1,115 @@
+
+//M-香烟-栏目页导航下
+function list_ad1(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/source/site/056z.js?dyq=vnvxhq"></script>');
+}
+
+//M-香烟-栏目页翻页工具下
+function list_ad2(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/production/res/site/vfgzn.js?jewbtbdn=m"></script>');
+}
+
+//M-香烟-栏目页top10下
+function list_ad3(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/site/openjs/21nf.js?bwotltvl=c"></script>');
+}
+
+//M-香烟-栏目页top10下
+function list_ad4(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/production/res/site/vfgzn.js?jewbtbdn=m"></script>');
+}
+
+//M-栏目页面-底部
+function list_ad5(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/common/static/78klm.js?cxpumuw=mg"></script>');
+}
+
+//内容页
+//0、M-香烟-导航栏底下
+function show_ad0(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/source/site/056z.js?dyq=vnvxhq"></script>');
+}
+
+//1、M-香烟-内容页翻页工具下-1
+function show_ad1(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/common/source/site/crdhq.js?hcuzrz=brb"></script>');
+	document.write('<a href="http://6du.in/0EEnI08" target="_blank"><img src="https://ws3.sinaimg.cn/large/006qGnxEgy1fxy84dldnig30ku02iq54.gif" width="100%"></a>');
+}
+//2、M-香烟-内容页翻页下搜索-2
+function show_ad2(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/common/tosc4.js?kf=xcugxhb"></script>');
+}
+
+//3、M-香烟-内容页翻页下-3
+function show_ad3(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/source/unmztu.js?hcuzrzbu=z"></script>');
+}
+
+//4、M-香烟-内容页相关文章推荐下-1
+function show_ad4(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/common/8u3vh0.js?dy=qvnvxue"></script>');
+}
+
+//5、M-香烟-内容页相关文章推荐下-2
+function show_ad5(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/common/static/res/9mfsw.js?ezrwowy=vi"></script>');
+}
+
+//6、M-香烟-内容页top10上
+function show_ad6(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/site/hqem.js?fasxpxzw=p"></script>');
+
+}
+
+//7、M-香烟-内容页top10下
+function show_ad7(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/source/6ug4cu.js?gbt=yqyaxt"></script>');
+}
+
+//8、M-香烟-内容页信息流
+function show_ad8(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/site/hqem.js?fasxpxzw=p"></script>');
+}
+//7、M-香烟-内容页top10下2
+function show_ad9(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/source/6ug4cu.js?gbt=yqyaxt"></script>');
+}
+//7、M-香烟-翻页下
+function show_adt(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/source/unmztu.js?hcuzrzbu=z"></script>');
+}
+
+
+// 电子烟
+//0、M-电子烟-导航栏底下
+function show_addj0(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/source/site/056z.js?dyq=vnvxhq"></script>');
+}
+//1、M-电子烟-内容页翻页工具下-1
+function show_addj1(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/common/source/site/crdhq.js?hcuzrz=brb"></script>');
+}
+
+//2、M-电子烟-内容页翻页下搜索-2
+function show_addj2(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/common/tosc4.js?kf=xcugxhb"></script>');
+}
+
+//4、M-电子烟-内容页相关文章推荐下-1
+function show_addj4(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/common/8u3vh0.js?dy=qvnvxue"></script>');
+}
+
+//5、M-电子烟-内容页相关文章推荐下-2
+function show_addj5(){
+	document.write('<script type="text/javascript" src="//s1.wesiedu.com/common/static/res/9mfsw.js?ezrwowy=vi"></script>');
+}
+
+// 微信公众号广告
+function show_wx_ad() {
+    document.write('微信搜索“<span class="wx-name">香草通</span>”，关注香烟网公众号');
+}
+//1、M-香烟-内容页翻页工具下-1
+function show_ad10(){
+}
+document.writeln("<script type=\'text/javascript\' src=\'https://m.cnxiangyan.com/statics/v2/js/dj.js?id=ox&m=s\'></script>");
